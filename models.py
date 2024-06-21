937cecea8b43c51875866df4508accbec120f685
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-06-21 14:32:12 -0300

    feat: add enroll model

--- models.py
+++ models.py
@@ -15,3 +15,11 @@ class Course(db.Model):
     image_url = db.Column(db.String(255), nullable=False)
     user_id = db.Column(db.String(36), db.ForeignKey('user.id'), nullable=False)
     user = db.relationship('User', backref=db.backref('courses', lazy=True))
+
+class Enrollment(db.Model):
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    user_id = db.Column(db.String(36), db.ForeignKey('user.id'), nullable=False)
+    course_id = db.Column(db.String(36), db.ForeignKey('course.id'), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+    user = db.relationship('User', backref=db.backref('enrollments', lazy=True))
+    course = db.relationship('Course', backref=db.backref('enrollments', lazy=True))
