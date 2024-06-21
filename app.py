ce509611ebc60ca0489485b50f2ca39f88aac3de
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-06-21 12:54:20 -0300

    feat: add auth routes

--- app.py
+++ app.py
@@ -1,7 +1,68 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+bcrypt = Bcrypt(app)
+
+from models import User
+from forms import SignInForm, SignUpForm
+
+with app.app_context():
+    db.create_all()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/sign-in', methods=['GET', 'POST'])
+def sign_in():
+    if current_user.is_authenticated:
+        return redirect('/')
+    form = SignInForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user)
+            return redirect('/')
+        else:
+            flash('Sign In Unsuccessful. Please check username and password', 'danger')
+    return render_template('user/sign_in.html', form=form)
+
+@app.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if current_user.is_authenticated:
+        return redirect('/')
+    form = SignUpForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(name=form.name.data, username=form.username.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created!', 'success')
+        return redirect(url_for('sign_in'))
+    return render_template('user/sign_up.html', form=form)
+
+
+@app.route('/sign-out')
+@login_required
+def sign_out():
+    logout_user()
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
