19ecf9c556e1173d8da33ddbaa67bc1246cd32c2
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-06-21 15:08:42 -0300

    feat: add lesson model

--- models.py
+++ models.py
@@ -23,3 +23,11 @@ class Enrollment(db.Model):
     completed = db.Column(db.Boolean, default=False)
     user = db.relationship('User', backref=db.backref('enrollments', lazy=True))
     course = db.relationship('Course', backref=db.backref('enrollments', lazy=True))
+
+class Lesson(db.Model):
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    title = db.Column(db.String(150), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    video_link = db.Column(db.String(255), nullable=False)
+    course_id = db.Column(db.String(36), db.ForeignKey('course.id'), nullable=False)
+    course = db.relationship('Course', backref=db.backref('lessons', lazy=True))
