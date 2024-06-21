7a314b1a5e5cfd91d583ff438ecbf37730bbad64
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-06-21 13:28:17 -0300

    feat: add course form

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Length, EqualTo
 
 class SignUpForm(FlaskForm):
@@ -13,3 +13,9 @@ class SignInForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=150)])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=150)])
     submit = SubmitField('Login')
+
+class CourseForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired(), Length(min=2, max=150)])
+    description = TextAreaField('Description', validators=[DataRequired(), Length(min=2, max=500)])
+    image_url = StringField('Image URL', validators=[DataRequired(), Length(min=2, max=255)])
+    submit = SubmitField('Create Course')
