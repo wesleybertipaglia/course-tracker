615680f7b3b1b140e2d9ade59170680aa037a6ec
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-06-21 13:28:04 -0300

    feat: add course model

--- models.py
+++ models.py
@@ -7,3 +7,11 @@ class User(db.Model, UserMixin):
     name = db.Column(db.String(150), nullable=False)
     username = db.Column(db.String(150), nullable=False, unique=True)
     password = db.Column(db.String(150), nullable=False)
+
+class Course(db.Model):
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    title = db.Column(db.String(150), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    image_url = db.Column(db.String(255), nullable=False)
+    user_id = db.Column(db.String(36), db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('courses', lazy=True))
